fdcae0ac96717e1730c0a7ee181afae9dc3e3d7e
Aryan Singh <74867817+aryansingh920@users.noreply.github.com>
2023-05-30 01:34:16 +0530

    Update ec2.py (#1)

--- ec2.py
+++ ec2.py
@@ -14,9 +14,46 @@ session = boto3.Session(
 ec2_client = session.client('ec2')
 
 
-@app.get("/get-public-ip")
-def get_public_ip():
+@app.get("/start-ec2")
+def start_ec2():
+    instance_id = 'your-instance-id'  # Replace with your instance ID
+    response = ec2_client.start_instances(InstanceIds=[instance_id])
+    return {"message": "EC2 instance starting"}
+
+
+@app.get("/stop-ec2")
+def stop_ec2():
+    instance_id = 'your-instance-id'  # Replace with your instance ID
+    response = ec2_client.stop_instances(InstanceIds=[instance_id])
+    return {"message": "EC2 instance stopping"}
+
+
+@app.get("/get-private-ip")
+def get_private_ip():
     instance_id = 'your-instance-id'  # Replace with your instance ID
     response = ec2_client.describe_instances(InstanceIds=[instance_id])
-    public_ip = response['Reservations'][0]['Instances'][0]['PublicIpAddress']
-    return {"public_ip": public_ip}
+    private_ip = response['Reservations'][0]['Instances'][0]['PrivateIpAddress']
+    return {"private_ip": private_ip}
+
+# from fastapi import FastAPI
+# import boto3
+
+# app = FastAPI()
+
+# # Configure AWS credentials
+# session = boto3.Session(
+#     aws_access_key_id='YOUR_AWS_ACCESS_KEY',
+#     aws_secret_access_key='YOUR_AWS_SECRET_ACCESS_KEY',
+#     region_name='us-west-2'  # Replace with your desired AWS region
+# )
+
+# # Create EC2 client
+# ec2_client = session.client('ec2')
+
+
+# @app.get("/get-public-ip")
+# def get_public_ip():
+#     instance_id = 'your-instance-id'  # Replace with your instance ID
+#     response = ec2_client.describe_instances(InstanceIds=[instance_id])
+#     public_ip = response['Reservations'][0]['Instances'][0]['PublicIpAddress']
+#     return {"public_ip": public_ip}
